dd416ccf8b17bacdc21a62e5018e352b8fcaf145
jwnasambu <wamalwa1844.com>
2025-05-13 16:46:47 +0300

    Add to cart react

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import Layout from './components/layout';
 import Home from './pages/home';
 import Detail from './pages/detail';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index element={<Home />} />
-            <Route path='/:slug' element={<Detail />} />
-          </Route>
-       </Routes>
+          <Route path='/:slug' element={<Detail />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
