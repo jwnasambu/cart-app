851e3f3c9e789dc0d3dd9d422b769c21fbb36791
jwnasambu <wamalwa1844.com>
2025-05-13 15:54:17 +0300

    Refactor the header page

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import iconCart from '../assets/images/iconCart.png'
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleStatusTab } from '../store/cart';
 
 const Header = () => {
     const [totalQuantity, setTotalQuantity] = useState(0);
     const carts = useSelector(store => store.cart.items);
+    const dispatch = useDispatch(); 
     useEffect(() => {
         let total = 0;
         carts.forEach(item => total += item.quantity);
         setTotalQuantity(total);
-      }, [carts])
+    }, [carts])
+    const handleOpenTabCart = () => {
+        dispatch(toggleStatusTab());
+     }
     return (
         <header className='flex justify-between items-center mb-5'>
             <Link to="/" className='text-xt font-semibold'>Home.</Link>
             <div className='w-10 h-10 bg-gray-100 rounded-full
-            flex justify-center items-center relative'>
+            flex justify-center items-center relative' onclick={handleOpenTabCart}>
                 <img src={iconCart} alt='' className='w-6' />
                 <span className='absolute top-2/3 right-1/2 bg-red-500 text-white text-sm
                 w-5 h-5 rounded-full flex justify-center items-center'>0</span>
